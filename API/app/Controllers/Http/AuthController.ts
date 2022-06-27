5fecbaed8d3fc2bc506bb32157caa7fae78e2461
Crislâine Santos <criis.santos@outlook.com>
2022-06-27 16:26:19 -0300

    feat(api): access login added

--- API/app/Controllers/Http/AuthController.ts
+++ API/app/Controllers/Http/AuthController.ts
@@ -1,9 +1,14 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import axios from 'axios'
 
 import User from 'App/Models/User'
 
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:4000',
+})
+
 export default class AuthController {
-  public async login({ auth, request, response }: HttpContextContract) {
+  public async loginAdmin({ auth, request, response }: HttpContextContract) {
     const email = request.input('email')
     const password = request.input('password')
 
@@ -17,5 +22,24 @@ export default class AuthController {
     } catch {
       return response.badRequest('Invalid credentials')
     }
+  }
+
+  public async loginClient({ auth, request, response }: HttpContextContract) {
+    const data = request.only(['email', 'password'])
+
+    const verifyCredentials = await axiosInstance.post('/clients/verify-credentials', data)
+
+    const client = verifyCredentials.data
+
+    if (!client) {
+      return response.badRequest({ message: 'Invalid credentials' })
+    }
+
+    const token = await auth.use('api').generate(client, {
+      expiresIn: '3hours',
+      name: client.email,
+    })
+
+    return response.ok({ token, user: client })
   }
 }
